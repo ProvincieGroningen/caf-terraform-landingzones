3639947915f3490cd442d8a0a3ea71b86188aa51
jvdhaterd <122307726+jvdhaterd@users.noreply.github.com>
2024-05-15 17:05:59 +0200

    Update dynamic_secrets.tf

--- caf_solution/dynamic_secrets.tf
+++ caf_solution/dynamic_secrets.tf
@@ -2,12 +2,15 @@ module "dynamic_keyvault_secrets" {
   source  = "aztfmod/caf/azurerm//modules/security/dynamic_keyvault_secrets"
   version = "5.7.11"
 
-  for_each = {
-    for keyvault_key, secrets in try(var.dynamic_keyvault_secrets, {}) : keyvault_key => {
-      for key, value in secrets : key => value
-      if try(value.value, null) == null
-    }
-  }
+  # Below code does not work for dynamic secrets and is not the same as in caf_launchpad where it does work...
+  #for_each = {
+  #  for keyvault_key, secrets in try(var.dynamic_keyvault_secrets, {}) : keyvault_key => {
+  #    for key, value in secrets : key => value
+  #    if try(value.value, null) == null
+  #  }
+  #}
+
+  for_each = try(var.dynamic_keyvault_secrets, {})
 
   settings = each.value
   keyvault = module.solution.keyvaults[each.key]
