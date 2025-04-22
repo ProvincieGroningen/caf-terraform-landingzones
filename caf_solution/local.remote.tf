f1e1b50e9bb765f093edb6c6f8117cbf37462caa
marcomansigroningen <160317120+marcomansigroningen@users.noreply.github.com>
2025-04-22 14:05:56 +0200

    add application_insights to local.remote.tf

--- caf_solution/local.remote.tf
+++ caf_solution/local.remote.tf
@@ -56,6 +56,9 @@ locals {
     application_gateway_applications_v1 = {
       for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].application_gateway_applications_v1, {}))
     }
+    application_insights = {
+      for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].application_insights, {}))
+    }
     availability_sets = {
       for key, value in try(var.landingzone.tfstates, {}) : key => merge(try(data.terraform_remote_state.remote[key].outputs.objects[key].availability_sets, {}))
     }
